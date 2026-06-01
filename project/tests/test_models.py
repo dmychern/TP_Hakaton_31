978518cfdc4a39928dd903f668e8bfb156cabdec
dmychern <dschernyshev9@gmail.com>
2026-06-01 15:31:10 +0000

    Создал папку tests. Написал тесты для класса EmailMessage

---
+++ project/tests/test_models.py
@@ -0,0 +1,23 @@
+import pytest
+from src.models import EmailMessage
+
+@pytest.fixture
+def message_example():
+    return EmailMessage("mail_0000.txt", "Тема", "Текст письма")
+
+def test_message_init(message_example):
+    assert message_example.filename == "mail_0000.txt"
+    assert message_example.subject == "Тема"
+    assert message_example.body == "Текст письма"
+
+@pytest.mark.parametrize("subject, body, expected", [
+    ("КАПС", "не капс", "капс не капс"),
+    ("КАПС", "КАПС", "капс капс"),
+    ("", "текст", " текст"),
+    ("тема", "", "тема ")
+])
+
+def test_message_get_text(subject, body, expected):
+    message = EmailMessage("mail_0000.txt", subject, body)
+    
+    assert message.get_full_text() == expected
