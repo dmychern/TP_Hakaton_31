4a9e48e2e2d2c2af724ba1977098eaf979588ecb
Argentrix <163351060+Argentrix@users.noreply.github.com>
2026-06-02 03:53:55 +0300

    Внесение послежних изменений и очистка кэша

--- project/run.sh
+++ project/run.sh
@@ -7,12 +7,12 @@ elif command -v python3 &> /dev/null; then
 fi
 
 if [ ! -d "inbox" ]; then
-  echo "Предупреждение: Директория 'inbox' не найдена, папка добавлена автоматически"
+  echo "Директория 'inbox' не найдена, папка добавлена автоматически"
   mkdir -p inbox
 fi
 
 if [ ! -d "data" ]; then
-  echo "Предупреждение: Директория 'data' не найдена, папка добавлена автоматически"
+  echo "Директория 'data' не найдена, папка добавлена автоматически"
   mkdir -p data
 fi
 
@@ -21,32 +21,32 @@ if [ ! -d "data/logs" ]; then
 fi
 
 if [ ! -d "data/Important" ]; then
-  echo "Предупреждение: Директория 'Important' не найдена, папка добавлена автоматически"
+  echo "Директория 'Important' не найдена, папка добавлена автоматически"
   mkdir -p data/Important
 fi
 
 if [ ! -d "data/Important/client_support" ]; then
-  echo "Предупреждение: Директория 'client_support' не найдена, папка добавлена автоматически"
+  echo "Директория 'client_support' не найдена, папка добавлена автоматически"
   mkdir -p data/Important/client_support
 fi
 
 if [ ! -d "data/Important/tech_support" ]; then
-  echo "Предупреждение: Директория 'tech_support' не найдена, папка добавлена автоматически"
+  echo "Директория 'tech_support' не найдена, папка добавлена автоматически"
   mkdir -p data/Important/tech_support
 fi
 
 if [ ! -d "data/Newsletter" ]; then
-  echo "Предупреждение: Директория 'Newsletter' не найдена, папка добавлена автоматически"
+  echo "Директория 'Newsletter' не найдена, папка добавлена автоматически"
   mkdir -p data/Newsletter
 fi
 
 if [ ! -d "data/Service" ]; then
-  echo "Предупреждение: Директория 'Service' не найдена, папка добавлена автоматически"
+  echo "Директория 'Service' не найдена, папка добавлена автоматически"
   mkdir -p data/Service
 fi
 
 if [ ! -d "data/Spam" ]; then
-  echo "Предупреждение: Директория 'Spam' не найдена, папка добавлена автоматически"
+  echo "Директория 'Spam' не найдена, папка добавлена автоматически"
   mkdir -p data/Spam
 fi
 
